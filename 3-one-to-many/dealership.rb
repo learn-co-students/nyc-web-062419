bcf904fe9a0674c80c47e6102daddc00a469d6e5
github username <github email address>
2019-06-26 14:45:43 -0400

    another one

--- 3-one-to-many/dealership.rb
+++ 3-one-to-many/dealership.rb
@@ -1,9 +1,23 @@
 class Dealership
     attr_accessor :name 
     attr_reader :city 
+
+    @@all = []
+    
     def initialize(name, city)
         @name = name 
         @city = city
+        Dealership.all << self 
     end
+
+    def cars
+        #Should return all of this instance of Dealership's cars 
+        #Call on my Car class and ask that class to give me a list of all cars that belongs to this dealership 
+        
+
+
+
+    end
+
 
 end
