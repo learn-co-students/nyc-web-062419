d985fb5e7603b7ee2d69fe9f5b6f6722b2dca0f3
github username <github email address>
2019-07-03 17:59:33 -0400

    active record

--- 6-intro-active-record/app/models/appointment.rb
+++ 6-intro-active-record/app/models/appointment.rb
@@ -1,3 +1,4 @@
-class Appointment
-    
+class Appointment < ActiveRecord::Base
+    belongs_to :doctor
+    belongs_to :patient
 end
--- 6-intro-active-record/app/models/patient.rb
+++ 6-intro-active-record/app/models/patient.rb
@@ -1,8 +1,5 @@
-class Patient
-
-   def initialize(name)
-
-   end
-
+class Patient < ActiveRecord::Base
+has_many :appointments 
+has_many :doctors, through: :appointments
  
 end
