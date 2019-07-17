c41d962ab433d9aa3ada9ba307410831ce44f022
github username <github email address>
2019-07-17 15:11:10 -0400

    typo

--- 11-intro-rails/rappers/config/routes.rb
+++ 11-intro-rails/rappers/config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  resources :rappers, except: [:index]
 
-
-  # get '/rappers', to: "rappers#index", as: "rappers"
-  # get '/rappers/new', to: "rappers#new", as: "rapper"
-  # get '/rappers/:id/edit', to: "rappers#edit"
-  # get '/rappers/:id', to: "rappers#show", as: "rapper"
-  # post '/rappers', to: "rappers#create"
+  get '/rappers', to: "rappers#index", as: "rappers"
+  get '/rappers/new', to: "rappers#new", as: "rapper"
+  get '/rappers/:id/edit', to: "rappers#edit"
+  get '/rappers/:id', to: "rappers#show", as: "rapper"
+  post '/rappers', to: "rappers#create"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
